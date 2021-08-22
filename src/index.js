2fa4cba231898555965d82e31e9ca3f90e3b96af
Muhammad Hashim <56976652+ProHashim@users.noreply.github.com>
2021-08-22 23:50:32 +0500

    Added code for rendering HTML

--- src/index.js
+++ src/index.js
@@ -11,6 +11,38 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+const mytable = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+)
+
+ReactDOM.render(<p>Another div</p>, document.getElementById("anotherdiv"))
+
+ReactDOM.render(mytable, document.getElementById("mytable"))
+
+// Lets use some more JSX
+
+const myhead = <h1>Hello I am with JSX </h1>
+
+ReactDOM.render(myhead, document.getElementById("myhead"))
+
+const headWithReact = React.createElement('h1', {}, "H1 With REACT")
+
+
+ReactDOM.render(headWithReact, document.getElementById("h1withreact"))
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
